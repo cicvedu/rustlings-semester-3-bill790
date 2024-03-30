6ca65b022f006e230f2356992eb4ef69cf86d63a
xc <bill790@163.com>
2024-03-30 21:54:11 +0800

    keep going 2

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Wrapper {
     value: u32,
@@ -15,6 +14,18 @@ struct Wrapper {
 impl Wrapper {
     pub fn new(value: u32) -> Self {
         Wrapper { value }
+    }
+}
+
+struct StringWrapper {
+    value: String,
+}
+
+impl StringWrapper {
+    pub fn new(value: &str) -> Self {
+        StringWrapper {
+            value: value.to_string(),
+        }
     }
 }
 
@@ -29,6 +40,6 @@ mod tests {
 
     #[test]
     fn store_str_in_wrapper() {
-        assert_eq!(Wrapper::new("Foo").value, "Foo");
+        assert_eq!(StringWrapper::new("Foo").value, "Foo");
     }
 }
